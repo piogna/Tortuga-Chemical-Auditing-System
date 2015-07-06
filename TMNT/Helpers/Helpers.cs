4784a5bdb7936fe89a8a029838e5203785b981f0
Paul Iogna <paul.iogna@gmail.com>
2015-07-06 13:57:38 -0400

    Added Helpers/Helpers.cs and static method to return current user department.

---
+++ TMNT/Helpers/Helpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using TMNT.Models;
+
+
+namespace TMNT.Helpers
+{
+    public class Helpers
+    {
+        public static string GetDepartmentCode()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            UserManager<ApplicationUser> manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var user = manager.FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            return (user.Department != null) ? user.Department.DepartmentCode : "";
+        }
+    }
+}
