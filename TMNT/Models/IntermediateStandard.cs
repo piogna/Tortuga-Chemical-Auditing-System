876c1bf2f25fd9737503e94c99bf314e229e45de
Drew Kennedy <kenneddr@sheridanc.on.ca>
2015-08-19 12:30:58 -0400

    Added display attributes to Intermediate Standard, and icons for Balance and Syringe verifications on the side menu.

--- TMNT/Models/IntermediateStandard.cs
+++ TMNT/Models/IntermediateStandard.cs
@@ -5,7 +5,7 @@ namespace TMNT.Models {
     public class IntermediateStandard {
         [Key]
         public int IntermediateStandardId { get; set; }
-
+        [Display(Name = "ID Code")]
         public string IdCode { get; set; }
         [Required, DataType(DataType.Date), Display(Name = "Date Created")]
         public DateTime DateCreated { get; set; }
@@ -13,7 +13,9 @@ namespace TMNT.Models {
         [Display(Name = "Replaced By")]
         public string ReplacedBy { get; set; }
         public int Amount { get; set; }
+        [Display(Name = "Created By")]
         public string CreatedBy { get; set; }
+        [Display(Name = "Last Modified By")]
         public string LastModifiedBy { get; set; }
         //foreign keys
         [Required]
