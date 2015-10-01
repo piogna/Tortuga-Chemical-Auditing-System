84feb5617598a7ae2f5d594c848630c9352d4f0e
Drew Kennedy <kenneddr@sheridanc.on.ca>
2015-10-01 10:30:21 -0400

    Changed around the columns on the datatable on the dashboard.

--- TMNT/Scripts/CustomMaxxamJS/execute-datatables.js
+++ TMNT/Scripts/CustomMaxxamJS/execute-datatables.js
@@ -17,6 +17,17 @@
                     { 'bSortable': false, 'aTargets': [0] }
                 ]
             });
+        } else if (table[i].id == "home-data-table") {
+            $('#home-data-table').dataTable({
+                "pageLength": 10,
+                "order": [[0, "asc"]],
+                "aoColumnDefs": [
+                    { 'bSortable': false, 'aTargets': [4] },
+                    { 'bSearchable': false, "aTargets": [4] }
+                ],
+                "aLengthMenu": [[10, 25], [10, 25]],
+                "iDisplayLength": 25
+            });
         }
     }
 });
