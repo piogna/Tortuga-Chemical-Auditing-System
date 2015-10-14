3ad754f91311886aa1acdf45b918ce8d62dd99c1
Drew Kennedy <kenneddr@sheridanc.on.ca>
2015-10-13 21:01:20 -0400

    Created a new Attribute for needing to change your password.

---
+++ TMNT/Filters/PasswordChangeAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using TMNT.Helpers;
+
+namespace TMNT.Filters {
+    [AttributeUsage(AttributeTargets.All)]
+    public class PasswordChangeAttribute : ActionFilterAttribute {
+        public override void OnActionExecuting(ActionExecutingContext filterContext) {
+            if (HelperMethods.GetCurrentUser().IsFirstTimeLogin) {
+                RouteValueDictionary redirectTargetDictionary = new RouteValueDictionary();
+                redirectTargetDictionary.Add("area", "");
+                redirectTargetDictionary.Add("action", "ChangePasswordFirstTime");
+                redirectTargetDictionary.Add("controller", "Manage");
+                filterContext.Result = new RedirectToRouteResult(redirectTargetDictionary);
+            }
+        }
+    }
+}
