4cf1a31d1550b972025f68af35e2dac798045f53
Drew Kennedy <kenneddr@sheridanc.on.ca>
2015-10-16 11:50:15 -0400

    Added display attributes to some Location properties.

--- TMNT/Models/Location.cs
+++ TMNT/Models/Location.cs
@@ -6,12 +6,15 @@ namespace TMNT.Models {
         [Key]
         public int LocationId { get; set; }
         public string LocationCode { get; set; }
+        [Display(Name = "Location Name")]
         public string LocationName { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
         public string Province { get; set; }
         public string PostalCode { get; set; }
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
+        [Display(Name = "Fax Number")]
         public string FaxNumber { get; set; }
         public string Website { get; set; }
 
