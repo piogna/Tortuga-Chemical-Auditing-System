2d9176295ed0fb1423c5c6e65e16490f1b8ad928
Drew Kennedy <kenneddr@sheridanc.on.ca>
2015-11-03 09:30:55 -0500

    Getting Volumetric devices prepared to start being coded.

---
+++ TMNT/Controllers/VolumetricController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TMNT.Models;
+
+namespace TMNT.Controllers {
+    public class VolumetricController : Controller {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Volumetric
+        public ActionResult Index() {
+            return View(db.Devices.ToList());
+        }
+
+        // GET: Volumetric/Details/5
+        public ActionResult Details(int? id) {
+            if (id == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Device device = db.Devices.Find(id);
+            if (device == null) {
+                return HttpNotFound();
+            }
+            return View(device);
+        }
+
+        // GET: Volumetric/Create
+        public ActionResult Create() {
+            return View();
+        }
+
+        // POST: Volumetric/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DeviceId,DeviceCode,IsVerified,DeviceType,Status")] Device device) {
+            if (ModelState.IsValid) {
+                db.Devices.Add(device);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(device);
+        }
+
+        // GET: Volumetric/Edit/5
+        public ActionResult Edit(int? id) {
+            if (id == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Device device = db.Devices.Find(id);
+            if (device == null) {
+                return HttpNotFound();
+            }
+            return View(device);
+        }
+
+        // POST: Volumetric/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DeviceId,DeviceCode,IsVerified,DeviceType,Status")] Device device) {
+            if (ModelState.IsValid) {
+                db.Entry(device).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(device);
+        }
+
+        // GET: Volumetric/Delete/5
+        public ActionResult Delete(int? id) {
+            if (id == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Device device = db.Devices.Find(id);
+            if (device == null) {
+                return HttpNotFound();
+            }
+            return View(device);
+        }
+
+        // POST: Volumetric/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id) {
+            Device device = db.Devices.Find(id);
+            db.Devices.Remove(device);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TMNT/Models/ViewModels/VolumetricViewModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TMNT.Models.ViewModels {
+    public class VolumetricIndexViewModel {
+
+        [Display(Name = "Volumetric ID")]
+        public int VolumetricId { get; set; }
+        [Display(Name = "Device Code")]
+        public string DeviceCode { get; set; }
+        public Department Department { get; set; }
+        [Display(Name = "Balance Used")]
+        public Device BalanceUsed { get; set; }
+        [Display(Name = "Is Verified")]
+        public bool IsVerified { get; set; }
+        [DataType(DataType.Date), Display(Name = "Last Verified")]
+        public DateTime LastVerified { get; set; }
+        [Display(Name = "Last Verified By")]
+        public string LastVerifiedBy { get; set; }
+        
+        public List<DeviceVerification> DeviceVerifications { get; set; }
+    }
+}
