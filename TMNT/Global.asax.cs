20d21fb6eb30d5ba7c70dad72998bee790b5ed07
Ian Lomas <lomasian@hotmail.ca>
2015-11-25 10:41:15 -0800

    Error handling added to global.asax

--- TMNT/Global.asax.cs
+++ TMNT/Global.asax.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Specialized;
 using System.IO;
+using System.Net;
 using System.Security.Authentication;
 using System.Web;
 using System.Web.Http;
@@ -18,35 +20,61 @@ namespace TMNT {
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
 
-        //protected void Application_Error(object sender, EventArgs e) {
-        //    var exception = Server.GetLastError();
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Server.ClearError();
+            var exception = Server.GetLastError();
+            System.Web.HttpBrowserCapabilities browser = Request.Browser;
+            String user = User.Identity.Name;
+            if (user == "")
+            {
+                user = "Anonymous";
+            }
+            String emailBody = "<Strong>Report Time: </Strong>" + DateTime.Now + "<br/><br/>";
+            emailBody += "<Strong>Browser: </Strong>" + browser.Type + ".0 <br/><br/>";
+            emailBody += "<Strong>User: </Strong>" + user + "<br/><br/>";
+            emailBody += "<Strong>Error Message: </Strong>" + exception.Message + "<br/><br/>";
+            emailBody += "<Strong>Stack Trace: </Strong><br>" + exception.StackTrace;
 
-        //    Server.ClearError();
-        //    var httpException = exception as HttpException;
+            using (var client = new WebClient())
+            {
+                var values = new NameValueCollection();
+                values["call"] = "sendEmail";
+                values["subject"] = "Tortuga UAT - " + DateTime.Now + " - Error";
+                values["message"] = emailBody;
+                values["to"] = "lomasian@hotmail.ca";
+                var response = client.UploadValues("http://kal-rul.com/PHP/AJAX.php", values);
+            }
+            Server.ClearError();
+            var httpException = exception as HttpException;
 
-        //    //Logging goes here
+            //Logging goes here
 
-        //    var routeData = new RouteData();
-        //    routeData.Values["controller"] = "Error";
-        //    routeData.Values["action"] = "Error";
+            var routeData = new RouteData();
+            routeData.Values["controller"] = "Error";
+            routeData.Values["action"] = "Error";
 
-        //    if (httpException != null) {
-        //        if (httpException.GetHttpCode() == 404) {
-        //            routeData.Values["action"] = "NotFound";
-        //        }
-        //        Response.StatusCode = httpException.GetHttpCode();
-        //    } else {
-        //        Response.StatusCode = 500;
-        //    }
+            if (httpException != null)
+            {
+                if (httpException.GetHttpCode() == 404)
+                {
+                    routeData.Values["action"] = "NotFound";
+                }
+                Response.StatusCode = httpException.GetHttpCode();
+            }
+            else
+            {
+                Response.StatusCode = 500;
+            }
 
-        //    // Avoid IIS7 getting involved
-        //    Response.TrySkipIisCustomErrors = true;
+            // Avoid IIS7 getting involved
+            Response.TrySkipIisCustomErrors = true;
 
-        //    // Execute the error controller
-        //    IController errorsController = new ErrorController();
-        //    HttpContextWrapper wrapper = new HttpContextWrapper(Context);
-        //    var rc = new RequestContext(wrapper, routeData);
-        //    errorsController.Execute(rc);
-        //}
+            // Execute the error controller
+            IController errorsController = new ErrorController();
+            HttpContextWrapper wrapper = new HttpContextWrapper(Context);
+            var rc = new RequestContext(wrapper, routeData);
+            errorsController.Execute(rc);
+        }
     }
 }
