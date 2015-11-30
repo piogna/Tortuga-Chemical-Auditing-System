d965a55b6976da0a16ee0013c2d7a9396a9aa7f8
Drew Kennedy <kenneddr@sheridanc.on.ca>
2015-11-30 10:08:24 -0500

    Balance summary done. Cleaned up some of our javascript.

--- TMNT/Scripts/CustomMaxxamJS/finalize-create-data-table.js
+++ TMNT/Scripts/CustomMaxxamJS/finalize-create-data-table.js
@@ -1,50 +1,13 @@
-﻿$(function () {
-    //fixing section 1 getting the appropriate classes
-    //$('#section-bar-1').addClass('section');
+﻿/*
+    Finalizing All Create Forms
+    Created By: Team Tortuga
+*/
 
-    //filling summary table
-    var inputs = $('.input-summary'), table = $('table.summary-table > tbody');
-
-    $('.btn-review').on('click', function (e) {
-        //give Number of Bottles a default value if its value is 0 or empty
-        if (!$('#NumberOfBottles').val()) {
-            $('#NumberOfBottles').val("1");
-        }
-
-        //absolutely prevent duplicate rows
-        if ($('table.summary-table > tbody > tr').length > 0) {
-            table.find("tr").remove();
-        }
-        var row = "";
-        for (var i = 0; i < inputs.length; i++) {
-            if (inputs[i].attributes.getNamedItem('name').textContent === "Devices") {
-                row += "<tr>" +
-                     "<td>" + inputs[i].attributes.getNamedItem('name').textContent.replace(/([A-Z])/g, ' $1').trim() + "</td><td>";
-                for (var z = 0; z < $('#device-selector').val().length; z++) {
-                    row += $('#device-selector').val()[z] + " ";
-                }
-                row += "</td></tr>";
-            } else {
-                if (inputs[i].attributes.getNamedItem('name').textContent === "MSDSNotes") {
-                    row += "<tr>" +//drilling into each element and retrieving the value of each "name" attribute
-                    "<td>SDS Notes</td><td>" + inputs[i].value + "</td>" +
-                    "</tr>";
-                } else {
-                    row += "<tr>" +//drilling into each element and retrieving the value of each "name" attribute
-                        "<td>" + inputs[i].attributes.getNamedItem('name').textContent.replace(/([A-Z])/g, ' $1').trim() + "</td><td>" + inputs[i].value + "</td>" +
-                        "</tr>";
-                }
-            }
-        }
-        table.append(row);
-    });
-
-    /* BELOW IS REPLACING ABOVE */
-    //finalConcentrationUnits = $('#FinalConcentration'), 
+$(function () {
     var summaryCofa = $('#summary-cofa'), summaryMsds = $('#summary-msds'), summarySafetyNotes = $('#summary-safety-notes'), summaryDevices = $('#summary-devices'),
         inputCofa = $('#CertificateOfAnalysis'), inputMsds = $('#MSDS'), inputSafetyNotes = $('#SafetyNotes'), inputMsdsNotes = $('#MSDSNotes'), inputsPartOne = $('.input-summary-pt1'),
         inputsPartTwo = $('.input-summary-pt2'), inputDevices = $('#device-selector'), summaryInfoPartOne = $('#summary-info-pt1'), summaryInfoPartTwo = $('#summary-info-pt2'),
-            summaryAppend;
+        numberOfBottles = $('#NumberOfBottles'), summaryAppend;
 
     $('.btn-review').on('click', function () {
         //reset the summary page
@@ -57,8 +20,8 @@
         summaryDevices.find("dd").remove();
 
         //give Number of Bottles a default value if its value is 0 or empty
-        if (!$('#NumberOfBottles').val()) {
-            $('#NumberOfBottles').val("1");
+        if (!numberOfBottles.val()) {
+            numberOfBottles.val("1");
         }
 
         //setting the file names
@@ -121,4 +84,54 @@
         });
         summaryInfoPartTwo.append(summaryAppend);
     });
+
+    /* special onclick handler for creating a balance */
+
+    var locationNames = $('#location-names'), departmentNames = $('#department-names'), subDepartmentNames = $('#SubDepartmentName'),
+        summaryLocation = $('#summary-location-name'), summaryDepartment = $('#summary-department-name'),
+        summarySubDepartment = $('#summary-sub-department');
+
+    $('#btn-create-balance').on("click", function () {
+        summaryAppend = "";
+        summaryInfoPartOne.find("dt").remove();
+        summaryInfoPartOne.find("dd").remove();
+        summaryInfoPartTwo.find("dt").remove();
+        summaryInfoPartTwo.find("dd").remove();
+
+        summaryLocation.text(locationNames.first().val());
+        summaryDepartment.text(departmentNames.first().val());
+        summarySubDepartment.text(subDepartmentNames.first().val())
+
+        inputsPartOne.each(function () {
+            if ($(this).val()) {
+                if ($(this).attr("id") === "NumberOfDecimals") {
+                    summaryAppend += "<dt>" + $(this).attr("name").replace(/([A-Z])/g, ' $1').trim() + ":</dt>" +
+                            "<dd>" + $(this).val() + " (" + decimalExample(parseFloat($(this).val())) + ")</dd>";
+                } else {
+                    summaryAppend += "<dt>" + $(this).attr("name").replace(/([A-Z])/g, ' $1').trim() + ":</dt>" +
+                            "<dd>" + $(this).val() + "</dd>";
+                }
+            }
+        });
+
+        summaryInfoPartOne.append(summaryAppend);
+        //reset for second column
+        summaryAppend = "";
+
+        inputsPartTwo.each(function () {
+            if ($(this).val()) {
+                summaryAppend += "<dt>" + $(this).attr("name").replace(/([A-Z])/g, ' $1').trim() + ":</dt>" +
+                        "<dd>" + $(this).val() + "</dd>";
+            }
+        });
+        summaryInfoPartTwo.append(summaryAppend);
+    });
+
+    function decimalExample(value) {
+        var text = "##.";
+        for (var i = 0; i < value; i++) {
+            text += "#";
+        }
+        return text;
+    }
 });
