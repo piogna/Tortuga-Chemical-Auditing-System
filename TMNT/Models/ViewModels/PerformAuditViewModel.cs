f56a5bd9adf61d34569ca1559bc6f5a5db3af308
Drew Kennedy <kenneddr@sheridanc.on.ca>
2015-12-03 12:34:20 -0500

    Sanitizing Audit.

--- TMNT/Models/ViewModels/PerformAuditViewModel.cs
+++ TMNT/Models/ViewModels/PerformAuditViewModel.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace TMNT.Models.ViewModels
-{
-    public class PerformAuditViewModel
-    {
+namespace TMNT.Models.ViewModels {
+    public class PerformAuditViewModel {
+        [Required(ErrorMessage = "The ID Code is Required")]
         public string IdCode { get; set; }
+        [Required(ErrorMessage = "The Chemical Type is Required")]
         public string Type { get; set; }
     }
 }
